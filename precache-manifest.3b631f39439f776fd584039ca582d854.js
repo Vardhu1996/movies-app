3bc011cd302be4d517790625f3c9bd40d1eb9127
Vardhu1996 <avsv.vardhaman@gmail.com>
2020-09-29 16:41:54 +0530

    Updates

---
+++ precache-manifest.3b631f39439f776fd584039ca582d854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84cda0a98349f6212dbabd30f9e7e24",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "9671fcffdda635716b70",
+    "url": "/movies-app/static/css/main.2e6b2e93.chunk.css"
+  },
+  {
+    "revision": "38db30a341af043024d8",
+    "url": "/movies-app/static/js/2.b301800d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies-app/static/js/2.b301800d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9671fcffdda635716b70",
+    "url": "/movies-app/static/js/main.c76274f3.chunk.js"
+  },
+  {
+    "revision": "c9d1eb1fda73cd4f8533",
+    "url": "/movies-app/static/js/runtime-main.964dbdf1.js"
+  }
+]);
